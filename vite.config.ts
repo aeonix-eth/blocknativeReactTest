876baa2e49469c2a03c271ff9079372e0a608aa2
aeonix-eth <a17c@proton.me>
2024-04-17 03:37:25 -0400

    1

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,57 @@
 import { defineConfig } from 'vite'
 
 import react from '@vitejs/plugin-react'
-import nodePolyfills from 'vite-plugin-node-stdlib-browser'
+import nodePolyfills from 'rollup-plugin-polyfill-node'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), nodePolyfills()],
+const MODE = process.env.NODE_ENV
+const development = MODE === 'development'
+
+export default {
+  // other config options
+  plugins: [
+    development &&
+      react(),
+      nodePolyfills({
+        include: ['node_modules/**/*.js', new RegExp('node_modules/.vite/.*js'), 'http', 'crypto']
+      })
+  ],
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',
+      stream: 'stream-browserify',
+      assert: 'assert',
+      zlib: 'browserify-zlib'
+    }
+  },
+  build: {
+    rollupOptions: {
+      external: ['@web3-onboard/*'],
+      plugins: [
+        nodePolyfills({ include: ['crypto', 'http'] }),
+        inject({ Buffer: ['Buffer', 'Buffer'] })
+      ]
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
+  },
   optimizeDeps: {
-    exclude: ['@ethersproject/hash', 'wrtc'],
-    include: ['js-sha3', '@ethersproject/bignumber']
+    exclude: ['@ethersproject/hash', 'wrtc', 'http'],
+    include: [
+      '@web3-onboard/core',
+      '@web3-onboard/gas',
+      '@web3-onboard/sequence',
+      'js-sha3',
+      '@ethersproject/bignumber'
+    ],
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis'
+      }
+    }
+  },
+  define: {
+    global: 'window'
   }
-})
+}
